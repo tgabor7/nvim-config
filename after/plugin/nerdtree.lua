f186443aea8caefa1ee8d2eacf8ccb8f787e4175
shitbox <shitbox@DESKTOP-2MIAFA1.localdomain>
2022-12-31 17:51:27 +0100

    Some changes

---
+++ after/plugin/nerdtree.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<C-t>", ":NERDTreeToggle<CR>")
+vim.keymap.set("n", "<C-n>", ":NERDTree<CR>")
+vim.keymap.set("n", "<C-f>", ":NERDTreeFind<CR>")
+vim.keymap.set("n", "<leader>n", ":NERDTreeFocus<CR>")
